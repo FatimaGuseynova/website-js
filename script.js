be5ec9ac3f56e532a89c5d7d5ffa014ab8a3b256
FatimaGuseynova <fatimagusejnova2@gmail.com>
2026-03-26 14:15:38 +0400

    slider

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ let newIcon = document.getElementById("newIcon")
 
 
 function menud() {
-    if (hamMenu.style.display === "block"){
+    if (hamMenu.style.display === "block") {
         hamMenu.style.display = "none"
         menuIcon.style.display = "block"
         newIcon.style.display = "none"
-    } 
-    else{
+    }
+    else {
         hamMenu.style.display = "block"
         menuIcon.style.display = "none"
         newIcon.style.display = "block"
@@ -34,15 +34,31 @@ function menud() {
 
 }
 
+// slider
+let fir = document.getElementById("fir")
+let sec = document.getElementById("sec")
+let thi = document.getElementById("thi")
+
+function next() {
+    if (fir.style.display == "block") {
+        fir.style.display = "none"
+        sec.style.display = "block"
+    }
+    else {
+        fir.style.display == "block"
+    }
+}
+
+
 // passport
 
 let passport = document.getElementById("passport")
 
-function passp(){
-    if (passport.style.display == "block"){
+function passp() {
+    if (passport.style.display == "block") {
         passport.style.display = "none"
     }
-    else{
+    else {
         passport.style.display = "block"
     }
 }
@@ -52,14 +68,14 @@ function passp(){
 let agreement = document.getElementById("agreement")
 let confirm = document.getElementById("confirm")
 
-function agree(){
-    if (agreement.style.backgroundColor === "rgb(37, 99, 235)"){
+function agree() {
+    if (agreement.style.backgroundColor === "rgb(37, 99, 235)") {
         agreement.style.backgroundColor = "white"
         agreement.style.borderColor = "#d1d5db"
         confirm.style.backgroundColor = "#d1d5db"
         confirm.style.color = "gray"
     }
-    else{
+    else {
         agreement.style.backgroundColor = "rgb(37, 99, 235)"
         agreement.style.borderColor = "rgb(37, 99, 235)"
         confirm.style.backgroundColor = "rgb(37, 99, 235)"
